b9543ebacc39b6eace06337dae67e06a80f4ef59
Roberto Rivera <rrivera@redbannermedia.com>
2024-07-01 02:13:47 -0400

    Create new migrations, models, and controllers for notebooks, author, notebook

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,24 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\JsonResponse;
 
 Route::post('/register',
-    function (Request $request): \Illuminate\Http\JsonResponse {
+    function (Request $request): JsonResponse {
         return (new AuthController())->registerUser($request);
 });
 
 Route::post('/login',
-    function (Request $request): \Illuminate\Http\JsonResponse {
+    function (Request $request): JsonResponse {
         return (new AuthController())->loginUser($request);
 });
 
-//TODO: add logout functionality
 Route::post('/logout',
-    function (Request $request): \Illuminate\Http\JsonResponse {
+    function (Request $request): JsonResponse {
         return (new AuthController())->logout($request);
 });
 
+// TODO: Develop after other endpoints
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
