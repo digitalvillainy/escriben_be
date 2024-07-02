f0375cd5549edff73b25aec6bdd800308a0e7e71
rrivera <rrivera@redbannermedia.com>
2024-07-02 14:56:20 -0400

    Made comments for later usage

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,41 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\JsonResponse;
 
+/*
+* Description: Creates a new user
+* Body: username, first_name, last_name, email, password
+*/
 Route::post('/register',
     function (Request $request): JsonResponse {
         return (new AuthController())->registerUser($request);
 });
 
+
+/*
+* Description: Will login a user and return a token
+* Body: email, password
+*/
 Route::post('/login',
     function (Request $request): JsonResponse {
         return (new AuthController())->loginUser($request);
 });
 
+/*
+* NOTE: Be sure to send bearer token in header
+    * Description: Will logout current authenticated user
+    * Body: authenticated user email
+    * Content-Type: application/json
+    * Authorization: Bearer <token>
+*/
 Route::post('/logout',
     function (Request $request): JsonResponse {
         return (new AuthController())->logout($request);
-});
+})->middleware('auth:sanctum');
 
 /*
 * NOTE: Be sure to send bearer token in header
+    * Description: Will get current authenticated user
+    * Content-Type: application/json
     * Authorization: Bearer <token>
 */
 Route::get('/user', function (Request $request) {
